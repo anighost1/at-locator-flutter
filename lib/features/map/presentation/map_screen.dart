aeaf87bf7a0a9c6b01a5a699074f812d53cffa61
anighost1 <anil.anil300@gmail.com>
2026-08-04 23:25:20 +0530

    build fix and flutter map added

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Map Screen',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(23.3441, 85.3096), // Ranchi
+        initialZoom: 15,
       ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.at_locator',
+        ),
+      ],
     );
   }
 }
