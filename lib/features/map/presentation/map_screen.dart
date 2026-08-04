67bf362c861ac06656b1f0cc7dece3d59226a0a9
anighost1 <anil.anil300@gmail.com>
2026-08-04 23:35:47 +0530

    Update map_screen.dart

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final MapController _mapController = MapController();
+
+  LatLng? _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Location service is disabled")),
+      );
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Location permission denied")),
+      );
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    final location = LatLng(position.latitude, position.longitude);
+
+    setState(() {
+      _currentLocation = location;
+    });
+
+    _mapController.move(location, 17);
+  }
 
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(23.3441, 85.3096), // Ranchi
+      mapController: _mapController,
+      options: MapOptions(
+        initialCenter: _currentLocation ?? const LatLng(23.3441, 85.3096),
         initialZoom: 15,
       ),
       children: [
@@ -17,6 +69,22 @@ class MapScreen extends StatelessWidget {
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'com.example.at_locator',
         ),
+
+        if (_currentLocation != null)
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: _currentLocation!,
+                width: 50,
+                height: 50,
+                child: const Icon(
+                  Icons.my_location,
+                  color: Colors.blue,
+                  size: 35,
+                ),
+              ),
+            ],
+          ),
       ],
     );
   }
