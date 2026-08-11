49f7b8168130b281ca604468d0afff68d91a2dd2
anighost1 <anil.anil300@gmail.com>
2026-08-12 00:12:42 +0530

    trip location update

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -5,7 +5,7 @@ class ApiEndpoints {
 
   static const login = "$baseUrl/api/auth/login";
   static const trips = "$baseUrl/api/trip";
-  static const createTrip = "$baseUrl/api/trip/";
+  static const createTrip = "$baseUrl/api/trip";
   static const ongoingTrip = "$baseUrl/api/trip/ongoing";
 
   static String endTrip(int tripId) => "$baseUrl/api/trip/end/$tripId";
